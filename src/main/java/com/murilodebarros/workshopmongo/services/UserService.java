1ab13cf5129194d7d52218b396d5bfeaf3fa607a
Murilo de Barros <murilomb2901@gmail.com>
2024-11-28 22:49:22 -0300

    Inserção de usuario com POST

--- src/main/java/com/murilodebarros/workshopmongo/services/UserService.java
+++ src/main/java/com/murilodebarros/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.murilodebarros.workshopmongo.domain.User;
+import com.murilodebarros.workshopmongo.dto.UserDTO;
 import com.murilodebarros.workshopmongo.repository.UserRepository;
 import com.murilodebarros.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,6 +27,14 @@ public class UserService {
 	        throw new ObjectNotFoundException("Objeto não encontrado");
 	    }	    
 	    return user.get();
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
  
