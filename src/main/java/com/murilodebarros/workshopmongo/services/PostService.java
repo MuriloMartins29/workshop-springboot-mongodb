1676afec2e6855d5833f1572859a481ed8c442de
Murilo de Barros <murilomb2901@gmail.com>
2024-11-30 02:42:33 -0300

    Obtendo post por id

---
+++ src/main/java/com/murilodebarros/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.murilodebarros.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.murilodebarros.workshopmongo.domain.Post;
+import com.murilodebarros.workshopmongo.repository.PostRepository;
+import com.murilodebarros.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    if (!user.isPresent()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }	    
+	    return user.get();
+	}
+	
+	
+}
+ 
