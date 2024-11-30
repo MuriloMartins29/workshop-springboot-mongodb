30e24ee98ab64c8452e2c4469b300caf4e4d9b15
Murilo de Barros <murilomb2901@gmail.com>
2024-11-30 03:08:42 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/murilodebarros/workshopmongo/domain/Post.java
+++ src/main/java/com/murilodebarros/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.murilodebarros.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.murilodebarros.workshopmongo.dto.AuthorDTO;
+import com.murilodebarros.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO authour; 
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,7 +76,14 @@ public class Post implements Serializable{
 	public void setAuthour(AuthorDTO authour) {
 		this.authour = authour;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
 
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,6 +100,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
