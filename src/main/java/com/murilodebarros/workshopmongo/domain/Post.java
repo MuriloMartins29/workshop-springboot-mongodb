4120e394e27ef092497784e0366a84dbb659c974
Murilo de Barros <murilomb2901@gmail.com>
2024-11-29 22:25:13 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/murilodebarros/workshopmongo/domain/Post.java
@@ -0,0 +1,92 @@
+package com.murilodebarros.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date date;
+	private String title;
+	private String body;
+	private User authour; 
+	
+	public Post() {
+	}
+
+	public Post(String id, Date date, String title, String body, User authour) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.authour = authour;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getAuthour() {
+		return authour;
+	}
+
+	public void setAuthour(User authour) {
+		this.authour = authour;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/murilodebarros/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.murilodebarros.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.murilodebarros.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+
+}
