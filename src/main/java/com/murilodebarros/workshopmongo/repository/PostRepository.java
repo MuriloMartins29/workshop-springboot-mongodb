7f7e75d09598cbc1386bd7da2e9033f3ec1e4d8a
Murilo de Barros <murilomb2901@gmail.com>
2024-12-01 16:35:28 -0300

    Consulta simples com @Query

--- src/main/java/com/murilodebarros/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/murilodebarros/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.murilodebarros.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.murilodebarros.workshopmongo.domain.Post;
@@ -11,6 +12,8 @@ import com.murilodebarros.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	List<Post> findByTitleContaining(String text);
 }
